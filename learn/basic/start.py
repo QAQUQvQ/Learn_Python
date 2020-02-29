c92f40ccf979960e665399e805c735b3ab9b8c93
wubo <496983824@qq.com>
2020-02-29 20:29:51 +0800

    basix

--- learn/basic/start.py
+++ learn/basic/start.py
@@ -42,9 +42,9 @@ c = True
 
 # 列表示例
 
-list1 = ['Google', 'Runoob', 1997, 2000];
-list2 = [1, 2, 3, 4, 5, 6, 7];
-list3 = ["a", "b", "c", "d"];
+list1 = ['Google', 'Runoob', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+list3 = ["a", "b", "c", "d"]
 print("list1[0]: ", list1[0])
 print("list2[1:5]: ", list2[1:5])
 
@@ -101,7 +101,7 @@ dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
 print("dict['Name']: ", dict['Name'])
 print("dict['Age']: ", dict['Age'])
 
-dict['Age'] = 8;  # 更新 Age
+dict['Age'] = 8  # 更新 Age
 dict['School'] = "菜鸟教程"  # 添加信息
 print("dict['Age']: ", dict['Age'])
 print("dict['School']: ", dict['School'])
@@ -111,168 +111,3 @@ dict.clear()  # 清空字典
 del dict  # 删除字典
 # print("dict['Age']: ", dict['Age'])
 # print("dict['School']: ", dict['School'])
-
-
-
-# Fibonacci series: 斐波纳契数列
-# 两个元素的总和确定了下一个数
-a, b = 0, 1
-while b < 10:
-    print(b)
-    a, b = b, a+b
-
-
-# 循环
-n = 100
-sum = 0
-counter = 1
-while counter <= n:
-    sum = sum + counter
-    counter += 1
-print("1 到 %d 之和为: %d" % (n, sum))
-
-
-# 无限循环
-# var = 1
-# while var == 1:  # 表达式永远为 true
-#     num = int(input("输入一个数字  :"))
-#     print("你输入的数字是: ", num)
-# print("Good bye!")
-
-
-sites = ["Baidu", "Google","Runoob","Taobao"]
-for site in sites:
-    if site == "Runoob":
-        print("菜鸟教程!")
-        break
-    print("循环数据 " + site)
-else:
-    print("没有循环数据!")
-print("完成循环!")
-
-
-# break语句
-for letter in 'Python':  # 第一个实例
-    if letter == 'h':
-        break
-    print('当前字母 :', letter)
-var = 10  # 第二个实例
-while var > 0:
-    print('当前变量值 :', var)
-    var = var - 1
-    if var == 5:  # 当变量 var 等于 5 时退出循环
-        break
-print("Good bye!")
-
-
-
-# continue语句
-for letter in 'Python':  # 第一个实例
-    if letter == 'h':
-        continue
-    print('当前字母 :', letter)
-var = 10  # 第二个实例
-while var > 0:
-    var = var - 1
-    if var == 5:
-        continue
-    print('当前变量值 :', var)
-print("Good bye!")
-
-var1 = 'Hello World!'
-var2 = "Python Runoob"
-
-print("var1[0]: ", var1[0])
-print("var2[1:5]: ", var2[1:5])
-
-a = "Hello"
-b = "Python"
-
-print("a + b 输出结果：", a + b)
-print("a * 2 输出结果：", a * 2)
-print("a[1] 输出结果：", a[1])
-print("a[1:4] 输出结果：", a[1:4])
-
-if ("H" in a):
-    print("H 在变量 a 中")
-else:
-    print("H 不在变量 a 中")
-
-if ("M" not in a):
-    print("M 不在变量 a 中")
-else:
-    print("M 在变量 a 中")
-
-print(r'\n')
-print(R'\n')
-
-
-
-
-list=[1,2,3,4]
-it = iter(list)    # 创建迭代器对象
-print (next(it))   # 输出迭代器的下一个元素
-print (next(it))
-
-list=[1,2,3,4]
-it = iter(list)    # 创建迭代器对象
-for x in it:
-    print (x, end=" ")
-
-
-class MyNumbers:
-    def __iter__(self):
-        self.a = 1
-        return self
-
-    def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
-
-
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-
-
-# 计算面积函数
-def area(width, height):
-    return width * height
-
-
-def print_welcome(name):
-    print("Welcome", name)
-
-print_welcome("Runoob")
-w = 4
-h = 5
-print("width =", w, " height =", h, " area =", area(w, h))
-
-
-# 可写函数说明
-sum = lambda arg1, arg2: arg1 + arg2
-
-# 调用sum函数
-print("相加后的值为 : ", sum(10, 20))
-print("相加后的值为 : ", sum(20, 20))
-
-total = 0  # 这是一个全局变量
-
-
-# 可写函数说明
-def sum(arg1, arg2):
-    # 返回2个参数的和."
-    total = arg1 + arg2  # total在这里是局部变量.
-    print("函数内是局部变量 : ", total)
-    return total
-
-
-# 调用sum函数
-sum(10, 20)
-print("函数外是全局变量 : ", total)
