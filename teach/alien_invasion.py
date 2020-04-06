2d0eccfae6f7f47a0cd62d40450931953a79c842
wubo <496983824@qq.com>
2020-04-06 15:38:42 +0800

    fix

--- teach/alien_invasion.py
+++ teach/alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 def run_game():
     pygame.init()
@@ -13,12 +14,16 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_event(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- teach/settings.py
+++ teach/settings.py
@@ -13,3 +13,7 @@ class Settings():
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 10
 
+        self.alien_speed_factor = 5
+        self.fleet_drop_speed = 30
+        self.fleet_direction = 1  # 1表示右移，-1表示左移
+
